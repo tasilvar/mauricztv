4ca5e79f0328aec516082fd4f9f578d7cb299b82
mjendraszczyk <michal.jendraszczyk@gmail.com>
2024-01-22 23:35:43 +0100

    wyprowadzenie skryput do certyfikatow do stopki

--- wp-content/themes/mauricztv/footer.php
+++ wp-content/themes/mauricztv/footer.php
@@ -134,6 +134,44 @@ if(is_page(48)) {
 				$( ".has-sub > .sub-menu, ul:not(.sub-menu) > .menu-item-has-children > .sub-menu" ).wrap( "<div class='sub-menu-scroll-wrapper'></div>" );
 			});
 
+
+
+			(function( $ ) {
+    $.fn.generatePdfAndClose = function() {
+        var $this = $(this),
+            element = document.getElementById('generate-pdf-and-close'),
+            orientation = $this.find('#pb-page').data('orientation'),
+            opt_orientation = (orientation == 'horizontal') ? 'landscape' : 'portrait',
+            opt = {
+            margin:0,
+            filename:     'certificate.pdf',
+            image:        { type: 'jpeg', quality: 1 },
+            html2canvas:  { scale: 2, useCORS: true },
+            jsPDF:        {format : 'a4', orientation  : opt_orientation}
+        };
+
+        $('.ui-icon-gripsmall-diagonal-se').hide()
+
+        html2pdf(element, opt)
+            .set({ html2canvas: { scale: 2, useCORS: true } })
+            .then(function(pdf) {
+            setTimeout(function () {
+                window.close()
+            }, 1000);
+        });
+    };
+}( jQuery ));
+
+
+jQuery( document ).ready(function() {
+    if ( jQuery( "#generate-pdf-and-close" ).length > 0) {
+        // setTimeout added because generatePdfAndClose will not have time to load
+        setTimeout(function(){
+            jQuery( "#generate-pdf-and-close" ).generatePdfAndClose();
+        }, 750);
+    }
+});
+
 		</script>
 
 </body>
