4ac5fcbe67b8cae53c6f9699955c457b9e1adafc
endees <mtslawinski.p@gmail.com>
2024-08-13 10:37:18 +0200

    merge

--- wp-content/plugins/cookie-law-info/lite/admin/webpack/lite.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-module.exports = {
-	mode: 'production',
-	entry: path.resolve(__dirname, '../../frontend/js/script.js'),
-	output: {
-		path: path.resolve(__dirname, '../../frontend/js/'),
-		filename: 'script.min.js',
-	},
-};
